bb97a0d00350610217684a0bad541e90e2ea6577
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2024-03-15 14:24:59 -0600

    Finished nth element array problem

--- Set1.ts
+++ Set1.ts
@@ -1,5 +1,4 @@
 // Copy an array a specified number of times. If there is no second argument, copy array twice.
-
 const copyArray = (arr: number[], num?: number): number[] => {
     let tempArr: number[] = [];
     let multipliedArr: number[] = [];
@@ -18,4 +17,22 @@ const copyArray = (arr: number[], num?: number): number[] => {
     }
 
     return multipliedArr;
+}
+
+
+// Get every nth element in array. Default nth value is 1.
+const nthElement = (arr: number[], num?: number): number[] => {
+    let nthArr: number[] = [];
+
+    if(typeof num === 'undefined' || num === 1) {
+        nthArr = arr;
+    }
+    else if(num !== 0) {
+        for(let i = num-1; i < arr.length; i += num) {
+            nthArr.push(arr[i]);
+        }
+    }
+
+    console.log(nthArr);
+    return nthArr;
 }
