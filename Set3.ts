fa13b20d86d73aa975217d8105452de31896cc8f
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2024-05-23 18:15:53 -0600

    Completed second problem in set

--- Set3.ts
+++ Set3.ts
@@ -11,4 +11,17 @@ const invertValues = (obj: object): object => {
 console.log(invertValues({1: 'a', 2: 'b', 3: 'c'}))
 
 
-// Count all occurrences of a value in an array.
+// Count all occurrences of a specified value in an array.
+const allOccurrences = (arr: (string | number)[], val: (string | number)): number => {
+    let count: number = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === val) {
+            count++;
+        }
+    }
+
+    return count;
+}
+console.log(allOccurrences([1, 1, 2, 1, 2, 3], 1))
+console.log(allOccurrences([1, 'no', 'no', 'yes', 2, 3], 'no'))
